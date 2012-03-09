f53137c82012ad63d3b97f8bbbe27592cf3653ae
oliwel <github@oliwel.de>
2012-03-09 18:51:11 +0100

    Add a new Feature "Wrapper" Connectors
    When using Config::Multi, each subtree connector can now
    access its own configuration (everything which is on the same
    level as the connector definition) through self->conn()->get

--- lib/Connector/Proxy.pm
+++ lib/Connector/Proxy.pm
@@ -10,8 +10,50 @@ use strict;
 use warnings;
 use English;
 use Moose;
+use Connector::Wrapper;
 
 extends 'Connector';
+
+has LOOPBACK => (
+    is => 'ro',
+    isa => 'Connector::Wrapper',
+    reader => 'conn',
+    required => 0,
+);
+
+around BUILDARGS => sub {
+    
+    my $orig = shift;
+    my $class = shift;
+
+    my $args = $_[0];
+
+    if (  ref($args) eq 'HASH'
+            && defined($args->{CONNECTOR})
+            && defined($args->{TARGET}) ) {
+                
+            $args->{LOOPBACK} = Connector::Wrapper->new( $args );                
+    }
+    
+    return $class->$orig(@_);
+    
+};
+
+# Transparently add support for arrayref pathspec
+around get => sub {
+    my $orig = shift;
+    my $class = shift;
+     
+    my $path = shift;
+      
+    # TODO - might be possible to have different delimiters
+    if (ref $path) {
+        $path = join( $class->DELIMITER() , @{$path} );
+    }
+     
+    return $class->$orig($path);
+    
+};
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
@@ -19,10 +61,18 @@ __PACKAGE__->meta->make_immutable;
 1;
 __END__
 
-=head 1 Name
+=head1 Name
 
 Connector
 
-=head 1 Description
+=head1 Description
 
 This is the base class for all Connector::Proxy implementations.
+
+=head1 Developer Info
+
+When creating the connector, all class attributes that have a corresponding config
+item are initialised with the given values.
+
+All configuration options, that are denoted on the same level as the connector 
+definition are accessible inside the class using  C<$self->conn()->get()>.
---
+++ lib/Connector/Wrapper.pm
@@ -0,0 +1,49 @@
+# Connector::Wrapper
+#
+# Wrapper class to filter access to a connector by a prefix 
+#
+# Written by Oliver Welter for the OpenXPKI project 2012
+#
+package Connector::Wrapper;
+
+use strict;
+use warnings;
+use English;
+use Moose;
+use Data::Dumper;
+
+has '_prefix' => ( is => 'ro', required => 1, init_arg => 'TARGET' );
+
+has 'BASECONNECTOR' => ( is => 'ro', required => 1, init_arg => 'CONNECTOR' );
+
+sub get {
+    
+    my $self = shift;
+    my $path = shift;
+       
+    # TODO - might be possible to have different delimiters   
+    if (ref $path) {
+        unshift @{$path}, $self->_prefix();
+        $path = join(".", @{$path} );
+    } elsif ($path) {
+        $path = $self->_prefix() . $self->BASECONNECTOR()->DELIMITER() .  $path;
+    } else {
+        $path = $self->_prefix();
+    }
+     
+    return $self->BASECONNECTOR()->get( $path );        
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+
+1;
+__END__
+
+=head 1 Name
+
+Connector
+
+=head 1 Description
+
+This is the base class for all Connector::Proxy implementations.
