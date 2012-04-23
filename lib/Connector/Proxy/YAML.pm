558d85ef892a934f2cb97459cb667fe0f6373229
oliwel <github@oliwel.de>
2012-04-24 00:32:00 +0200

    Add new API methods and test script to Connector::Proxy::YAML

--- lib/Connector/Proxy/YAML.pm
+++ lib/Connector/Proxy/YAML.pm
@@ -15,57 +15,109 @@ use Data::Dumper;
 use Moose;
 extends 'Connector::Proxy';
 
-has 'allowhash' => (
-    is  => 'ro',
-    isa => 'Bool',    
-    default => 0,
-);
-
-
 sub _build_config {
     my $self = shift;
 
-    my $config = YAML::LoadFile($self->LOCATION());
+    my $config = YAML::LoadFile( $self->LOCATION() );
     $self->_config($config);
 }
 
-
-sub get {
+sub _get_node {
+    
     my $self = shift;
     my @path = $self->_build_path_with_prefix( shift );
 
     my $ptr = $self->_config();
 
-    while (scalar @path > 1) {
-	my $entry = shift @path;
-	if (exists $ptr->{$entry}) {
-	    if (ref $ptr->{$entry} eq 'HASH') {	
-		$ptr = $ptr->{$entry};
-	    } else {
-		  return $self->_node_not_exists(ref $ptr->{$entry} );
-	    }
-	} else {
-	    return $self->_node_not_exists( $entry );	    
-	}
+    while ( scalar @path > 1 ) {
+        my $entry = shift @path;
+        if ( exists $ptr->{$entry} ) {
+            if ( ref $ptr->{$entry} eq 'HASH' ) {
+                $ptr = $ptr->{$entry};
+            }
+            else {
+                return $self->_node_not_exists( ref $ptr->{$entry} );
+            }
+        } else {
+            return $self->_node_not_exists($entry);
+        }
     }
     
-    my $entry = $ptr->{shift @path};
+    return $ptr->{ shift @path };
     
-    # Inner Node    
-    if (ref $entry eq 'HASH') {
-        # List context - return keys
-        if (wantarray) {
-            return keys %{$entry};  
-        # Scalar but hash allowed - return Hash  
-        } elsif ($self->allowhash()) {
-            return $entry;
-        } 
-        # Default - size of keys
-        return scalar keys %{$entry};        
-    }
-    
-    return $entry;
 }
+
+sub get {
+    
+    my $self = shift;    
+    my $value = $self->_get_node( shift );
+    
+    return undef unless (defined $value);
+    
+    if (ref $value ne '') {
+        die "requested value is not a scalar"
+    }
+    
+    return $value;
+    
+}
+
+sub get_size {
+    
+    my $self = shift;    
+    my $node = $self->_get_node( shift );
+    
+    return undef unless(defined $node);
+    
+    if ( ref $node ne 'ARRAY' ) {
+        die "requested value is not a list"
+    }
+    
+    return scalar @{$node};    
+}
+
+
+sub get_list {
+    
+    my $self = shift;    
+    my $node = $self->_get_node( shift );
+    
+    return undef unless(defined $node);
+    
+    if ( ref $node ne 'ARRAY' ) {
+        die "requested value is not a list"
+    }
+    
+    return $node;   
+}
+
+sub get_keys {
+    
+    my $self = shift;    
+    my $node = $self->_get_node( shift );
+    
+    return undef unless(defined $node);
+    
+    if ( ref $node ne 'HASH' ) {
+        die "requested value is not a hash"
+    }
+    
+    return [ keys %{$node} ];   
+}
+
+sub get_hash {
+    
+    my $self = shift;    
+    my $node = $self->_get_node( shift );
+    
+    return undef unless(defined $node);
+    
+    if ( ref $node ne 'HASH' ) {
+        die "requested value is not a hash"
+    }
+    
+    return $node;   
+} 
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
--- t/01-proxy-yaml.t
+++ t/01-proxy-yaml.t
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use English;
 
-use Test::More tests => 11;
+use Test::More tests => 17;
 
 diag "LOAD MODULE\n";
 
@@ -45,5 +45,18 @@ is($conn->get( [ 'test.entry','bar' ] ), '5678');
 
 # check for completely wrong entry
 is($conn->get('test1.entry.bar'), undef, 'handle completely wrong entry gracefully');
+
+diag "Test List functionality\n";
+is( $conn->get_size('list.test'), 4, 'Check size of list');
+is( ref $conn->get_list('list.test'), 'ARRAY', 'Check if return is array ref');
+is( $conn->get_list('list.test')->[0], 'first', 'Check element');
+
+diag "Test Hash functionality\n";
+is( ref $conn->get_keys('test.entry'), 'ARRAY', 'Check if get_keys is array ');
+is( ref $conn->get_hash('test.entry'), 'HASH', 'Check if get_hash is hash');
+is( $conn->get_hash('test.entry')->{bar}, '5678', 'Check element');
+
+
+ 
 
 
